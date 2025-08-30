b5a5cb141ee563556e7c767ced7e3b394eb8ecb5
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-08-30 13:55:56 +0200

    building bones for the script

--- success_failure_ratio_visual.py
+++ success_failure_ratio_visual.py
@@ -2,4 +2,26 @@ from success_failure_ratio import SuccessRatio
 import matplotlib as plt
 
 
+class VisualizeRatio:
+    """"""
 
+    def __init__(self):
+        """"""
+
+    def _get_ratio_and_amount_numbers(self):
+        """"""
+        self.get_success, self.get_failure = input("What is the success/failure ratio (ex. 1/2)\n> ").split("/")
+        self.get_amount = input("What is the number of attempts?\n> ")
+        self._change_str_to_int(self.get_success, self.get_failure, self.get_amount)
+        print(type(self.get_success))
+    
+    def _change_str_to_int(self, *args):
+        """"""
+        for each in args:
+            int(each)
+            print(each)
+            print(type(each))
+
+
+check = VisualizeRatio()
+check._get_ratio_and_amount_numbers()
